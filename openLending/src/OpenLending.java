5511ac92184ccd0afb65e5bc8afda159f455d28a
Jennifer Nguyen <jennifer@useindio.com>
2023-07-26 19:04:18 -0500

    Open a browser, go to google, and search for OpenLending

--- openLending/src/OpenLending.java
+++ openLending/src/OpenLending.java
@@ -1,4 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenLending {
     public static void main(String[] args) throws Exception {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+        // Go to Google and search for OpenLending
+        driver.get("https://www.google.com");
+        WebElement searchbar = driver.findElement(By.name("q"));
+        searchbar.sendKeys("OpenLending");
+        searchbar.sendKeys(Keys.RETURN);
     }
 }
