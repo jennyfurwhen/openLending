6a89aff8b448ab9673172494a5c217d95e97839d
Jennifer Nguyen <jennifer@useindio.com>
2023-07-26 19:47:16 -0500

    Load all blogs by clicking on the Load More button until it no longer appears

--- openLending/src/OpenLending.java
+++ openLending/src/OpenLending.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,7 +12,7 @@ public class OpenLending {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        
+
         // Go to Google and search for OpenLending
         driver.get("https://www.google.com");
         WebElement searchbar = driver.findElement(By.name("q"));
@@ -26,5 +27,20 @@ public class OpenLending {
         String resourcesXpath = "//div[@id='main_nav']//a[text()='Resources']";
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(resourcesXpath)));
         driver.findElement(By.xpath(resourcesXpath)).click();
+
+        // While Load More button is present, scroll to it and click on it
+        // wait for Loading... button to appear and disappear before checking for the Load More button
+        // Load More button is not displayed (but is still present as an element) when there are no more blogs to load
+        String loadMoreButtonXpath = "//section[@id='featured-box-section']//button[text()='Load More']";
+        String loadingButtonXpath = "//section[@id='featured-box-section']//button[text()='Loading...']";
+        Actions actions = new Actions(driver);
+        while (driver.findElement(By.xpath(loadMoreButtonXpath)).isDisplayed()) {
+            WebElement loadMoreButton = driver.findElement(By.xpath(loadMoreButtonXpath));
+            actions.moveToElement(loadMoreButton);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath(loadMoreButtonXpath)));
+            loadMoreButton.click();
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(loadingButtonXpath)));
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(loadingButtonXpath)));
+        }
     }
 }
